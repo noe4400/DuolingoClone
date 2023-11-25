838c0829ce5b7b6ba4587711243581f833724734
fgarcia <gfabian757@gmail.com>
2023-11-25 10:55:16 -0600

    chore:add absolute paths config

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,19 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['.'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          '@components': './src/components',
+          '@constants': './src/constants',
+          '@screens': './src/screens',
+          '@utils': './src/utils',
+          '@assets': './src/assets',
+        },
+      },
+    ],
+  ],
 };
